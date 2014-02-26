8ffb43af1c19a6ef441a386497f42cbcffa514d9
Pete Dring <pddring@googlemail.com>
2014-02-26 14:11:38 +0000

    Added text examples

---
+++ 05-text-and-images.py
@@ -0,0 +1,86 @@
+# pygame example: github.com/pddring/pygame-examples
+
+
+"""
+This example shows you how to display text and combine it with images
+in the pygame graphics window.
+
+Things to try:
+TODO: Change the "Press Q to quit" from red to blue
+TODO: Make "Press Q to quit" appear at the bottom of the screen in the middle
+TODO: "Press any key..." is displayed nice and smoothly but "Press Q..." is
+        all blotchy. This is because "Press any key..." uses anti-aliasing.
+        Make "Press Q..." use anti-aliasing too so that it displays smoothly too.
+TODO: Add a joker to the pack
+
+"""
+# import modules
+import pygame
+import random
+
+# wait for the user to press a key and return the keycode
+def wait_for_key():
+    e = pygame.event.wait()
+    while e.type != pygame.KEYDOWN:
+        e = pygame.event.wait()
+        if e.type == pygame.QUIT:
+            return pygame.K_ESCAPE
+    return e.key
+
+
+# show the pygame window
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pygame Example")
+
+# load images
+img_card = pygame.image.load("images/card-back.png")
+img_heart = pygame.image.load("images/card-heart.png")
+img_club = pygame.image.load("images/card-club.png")
+img_spade = pygame.image.load("images/card-club.png")
+img_diamond = pygame.image.load("images/card-diamond.png")
+
+images_suits = [img_heart, img_club, img_spade, img_diamond]
+
+# create a font
+font = pygame.font.Font(None, 36)
+
+# use the font to make an image containg some text
+text = font.render("Press any key to show a random card", True, (0, 0, 0))
+text_quit = font.render("Press Q to quit", False, (255, 0, 0))
+
+# loop around until the user presses escape or Q
+looping = True
+while looping:
+    
+    # fill the screen in white
+    screen.fill((255, 255, 255))
+
+    # display the card
+    screen.blit(img_card, (100,100))
+    screen.blit(text, (0,0))
+    screen.blit(text_quit, (0,400))
+    
+    # update the screen
+    pygame.display.flip()
+
+    # display a white rectangle over the centre of the card
+    pygame.draw.rect(screen, (255,255,255), (110, 110, 180, 260), 0)
+
+    # choose a suit at random
+    suit = random.choice(images_suits)
+    screen.blit(suit, (130, 150))
+
+    # choose a number at random
+    number = random.choice(["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"])
+    screen.blit(font.render(number, 1, (0, 0, 0)), (120, 120))
+    pygame.display.flip()
+
+    key = wait_for_key()
+
+    # stop looping if the user presses Q or escape
+    if key == pygame.K_q or key == pygame.K_ESCAPE:
+        looping = False
+
+
+pygame.quit()
---
+++ 06-text-in-different-fonts.py
@@ -0,0 +1,65 @@
+# pygame example: github.com/pddring/pygame-examples
+
+
+"""
+This example shows how to display text in different colours and fonts
+
+
+Things to try:
+TODO: change the code so that all the text is red
+TODO: change the code so that it displays "One", "Two" up to "Ten" rather than "Hello"
+TODO: change the code so that the text appears in a different font
+TODO: change the code so that the background colour is blue
+TODO: change the code so that it displays 100 words instead of 50
+"""
+# import modules
+import pygame
+import random
+import time
+
+
+# show the pygame window
+pygame.init()
+screen = pygame.display.set_mode((800,600))
+pygame.display.set_caption("Pygame Example")
+
+# load the font
+""" 
+for this to work, you'll need to download the font from: 
+http://www.1001freefonts.com/five_dozen.font and extract FiveDozen.ttf into the same 
+folder as this python file.
+"""
+try:
+    # ideally, try to load a fancy font
+    font = pygame.font.Font("FiveDozen.ttf", 36)
+except:
+    # if FiveDozen.ttf can't be found, just a boring font instead
+    print "Download and extract FiveDozen.ttf from http://www.1001freefonts.com/five_dozen.font and save it in this folder to use that font"
+    font = pygame.font.Font(None, 36)
+
+# list of messages
+messages = ["Hello", "Goodbye", "Bonjour", "A bientot", "Hi", "Salut"]
+
+
+# fill the screen in white
+screen.fill((255, 255, 255))
+
+# loop 50 times
+for i in range(50):
+    # choose a random messag
+    msg = random.choice(messages)
+    
+    # create the text image in a random colour
+    text = font.render(msg, True, (random.randint(0, 255),
+                                   random.randint(0,255),
+                                   random.randint(0,255)))
+    
+    # display the text in a random position
+    screen.blit(text, (random.randint(0, 800),random.randint(0, 600)))
+    
+    # update the display and wait for 100ms (0.1s)
+    pygame.display.flip()
+    time.sleep(0.1)
+
+
+pygame.quit()
